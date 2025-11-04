b2248b43ca629334340c1ef029646fc7a0cd5b2d
Evokoo <evanjohncooper@gmail.com>
2025-11-05 02:02:42 +1100

    File generation improved

--- cmd/generate/quest.go
+++ cmd/generate/quest.go
@@ -21,7 +21,7 @@ func main() {
 		log.Fatal("Run from root")
 	}
 
-	day := getQuest()
+	quest := getQuest()
 	fileList := []File{
 		{fileName: "example_I.txt", content: "", open: true},
 		{fileName: "example_II.txt", content: "", open: false},
@@ -29,27 +29,27 @@ func main() {
 		{fileName: "input_I.txt", content: "", open: true},
 		{fileName: "input_II.txt", content: "", open: false},
 		{fileName: "input_III.txt", content: "", open: false},
-		{fileName: fmt.Sprintf("%s_suite_test.go", day), content: loadTemplate("cmd/generate/testSuite.tmpl", day), open: false},
-		{fileName: fmt.Sprintf("%s_test.go", day), content: loadTemplate("cmd/generate/test.tmpl", day), open: true},
-		{fileName: fmt.Sprintf("%s_logic.go", day), content: loadTemplate("cmd/generate/logic.tmpl", day), open: true},
-		{fileName: fmt.Sprintf("%s.go", day), content: loadTemplate("cmd/generate/solver.tmpl", day), open: true},
+		{fileName: fmt.Sprintf("%s_suite_test.go", quest), content: loadTemplate("cmd/generate/testSuite.tmpl", quest), open: false},
+		{fileName: fmt.Sprintf("%s_test.go", quest), content: loadTemplate("cmd/generate/test.tmpl", quest), open: true},
+		{fileName: fmt.Sprintf("%s_logic.go", quest), content: loadTemplate("cmd/generate/logic.tmpl", quest), open: true},
+		{fileName: fmt.Sprintf("%s.go", quest), content: loadTemplate("cmd/generate/solver.tmpl", quest), open: true},
 	}
 
-	createFolder(day)
+	createFolder(quest)
 	createFiles(fileList)
 }
 func getQuest() string {
-	day := flag.String("quest", "", "Which quest are you creating?")
+	quest := flag.String("quest", "", "Which quest are you creating?")
 	flag.Parse()
 
-	if *day == "" {
+	if *quest == "" {
 		log.Fatal("error: -quest flag is required")
 	}
-	return *day
+	return *quest
 }
-func createFolder(day string) {
+func createFolder(quest string) {
 	//Make the folder
-	err := os.Mkdir(day, 0755)
+	err := os.Mkdir(quest, 0755)
 	if err != nil {
 		log.Fatalf("Failed to create quest folder: %v", err)
 	}
@@ -58,19 +58,19 @@ func createFolder(day string) {
 	if err != nil {
 		log.Fatalf("Failed to get current working directory: %v", err)
 	} else {
-		fmt.Printf("%s folder created at %s\n", day, dir)
+		fmt.Printf("%s folder created at %s\n", quest, dir)
 	}
 
 	//Move into folder
-	err = os.Chdir(day)
+	err = os.Chdir(quest)
 	if err != nil {
 		log.Fatalf("Failed to change directory: %v", err)
 	}
 
-	//Create data folder
+	//Create inputs folder
 	err = os.Mkdir("inputs", 0755)
 	if err != nil {
-		log.Fatalf("Failed to create data folder: %v", err)
+		log.Fatalf("Failed to create inputs folder: %v", err)
 	}
 
 	dir, err = os.Getwd()
@@ -108,12 +108,12 @@ func createFiles(files []File) {
 		file.Close()
 	}
 }
-func loadTemplate(template, day string) string {
+func loadTemplate(template, quest string) string {
 	data, err := os.ReadFile(template)
 	if err != nil {
 		log.Fatalf("Failed to read template: %v", err)
 	}
-	return strings.ReplaceAll(string(data), "$", day)
+	return strings.ReplaceAll(string(data), "$", quest)
 }
 func rootCheck() bool {
 	dir, err := os.Getwd()
